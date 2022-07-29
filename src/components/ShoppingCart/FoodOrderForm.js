afb4f5ca6b9575360d6351b570bbd63c68f202c2
MatHyp <matijoker29@gmail.com>
2022-07-29 17:27:11 +0200

    add useReducer for form inputs

--- src/components/ShoppingCart/FoodOrderForm.js
+++ src/components/ShoppingCart/FoodOrderForm.js
@@ -7,22 +7,51 @@ const initialFormState = {
     name: "",
     street: "",
     postCode: "",
-    city: ""
+    city: '',
+}
+
+const types = {
+    name: 'changeName',
+    street: 'changeStret',
+    postCode: 'changeCode',
+    city: 'changeCity',
+}
+
+
+const reducer = (state,action) =>{
+    switch(action.type){
+        case  types.name:
+            return {...state, name: action.value}
+        case types.street:
+            return {...state, street: action.value}
+        case types.postCode:
+            return {...state, postCode: action.value}
+        case types.city:
+            return {...state, city: action.value}
+    }
+
 }
 
 const FoodOrderForm = () =>{
-    const [order, dispatch] = useReducer(FoodOrderFormReducer, initialFormState)    
+    const [state, dispatch] = useReducer(reducer, initialFormState)
+    
+    const onChangeFormHandler = (e, dependency) => {
+        dispatch({type: dependency , value: e.target.value});
+    }
+    const onFormSubmit = (e) => {}
+
 
     return <div>
-        <form>
+        <form onSubmit={onFormSubmit}>
             <p className='labels'>Your Name</p>
-            <input type='text' className="labels"/>
+            <input type='text' className="labels" value={state.name}  onChange={e => {onChangeFormHandler(e, types.name)}}/>
             <p className='labels'>Street</p>
-            <input type='text' className="labels"/>
+            <input type='text' className="labels" value={state.street} onChange={e =>onChangeFormHandler(e, types.street)}/>
             <p className='labels'>Postal Code</p>
-            <input type='text' className="labels"/>
+            <input type='text' className="labels" value={state.postCode}  onChange={e => onChangeFormHandler(e, types.postCode)}/>
             <p className='labels'>City</p>
-            <input type='text' className="labels"/>
+            <input type='text' className="labels" value={state.city} onChange={e => onChangeFormHandler(e, types.city)}/>
+            <input type='submit'/>
         </form>
     </div>
 }
